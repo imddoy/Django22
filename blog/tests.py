e411eeb336ea7512db23cddd5e9099d29d0c6a95
ddddddoii <imddoy@duksung.ac.kr>
2022-10-18 15:31:09 +0900

    포스트 상세페이지 테스트

--- blog/tests.py
+++ blog/tests.py
@@ -16,7 +16,7 @@ class TestView(TestCase):
         soup=BeautifulSoup(response.content, 'html.parser')
 
         #title이 정상적으로 보이는지
-        self.assertEqual(soup.title.text, 'Blog')
+        self.assertEqual(soup.title.text, 'blog')
 
         #navbar가 정상적으로 보이는지
         navbar = soup.nav
@@ -32,12 +32,31 @@ class TestView(TestCase):
         #2. post가 추가
         post_001 = Post.objects.create(title="첫번째 포스트", content="첫번째 포스트입니다.")
         post_002 = Post.objects.create(title="두번째 포스트", content="두번째 포스트입니다.")
-        self.assertEqual(Post.objects.count(), 0)
+        self.assertEqual(Post.objects.count(), 2)
 
         response = self.client.get('/blog/', follow=True)  # 301
         self.assertEqual(response.status_code, 200)
         soup = BeautifulSoup(response.content, 'html.parser')
         main_area = soup.find('div', id="main-area")
-        self.assertIn(post_001, main_area.text)
-        self.assertIn(post_002, main_area.text)
-        self.asserNotIn('아직 게시물이 없습니다.',main_area.text)
+        self.assertIn(post_001.title, main_area.text)
+        self.assertIn(post_002.title, main_area.text)
+        self.assertNotEqual('아직 게시물이 없습니다.',main_area.text)
+
+    def test_post_detail(self):
+        post_001 = Post.objects.create(title="첫번째 포스트", content="첫번째 포스트입니다.")
+        self.assertEqual(post_001.get_absolute_url(), '/blog/1/')
+
+        response = self.client.get(post_001.get_absolute_url(), follow=True)  # 301 #response = self.client.get('/blog/1/', follow=True)  # 301
+        self.assertEqual(response.status_code,200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        nav = soup.nav
+        self.assertIn('blog', nav.text)
+        self.assertIn('about me', nav.text)
+
+        self.assertIn(post_001.title,soup.title.text)
+        
+        main_area = soup.find('div', id='main-area')
+        post_area = main_area.find('div', id='post-area')
+        self.assertIn(post_001.title, post_area.text)
+        self.assertIn(post_001.content, post_area.text)
